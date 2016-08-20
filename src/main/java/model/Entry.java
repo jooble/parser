6e209ec9a780d9d0d9c8738499100f2f9507cf02
Дмитрий Жезлов <d.zhezlov@outlook.com>
2016-08-20 16:15:35 +0300

    generate class from xsd

--- src/main/java/model/Entry.java
+++ src/main/java/model/Entry.java
@@ -1,21 +1,32 @@
 package model;
 
 import javax.persistence.*;
-import java.util.Date;
+import javax.xml.bind.annotation.*;
+import javax.xml.datatype.XMLGregorianCalendar;
 
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "content",
+        "creationDate"
+})
+@XmlRootElement(name = "Entry")
 @Entity
 @Table(name = "entry")
 public class Entry {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "entry_id")
     private long id;
 
     @Column(name = "content", length = 1024)
+    @XmlElement(required = true)
     private String content;
 
     @Column(name = "creation_date")
-    private Date creationDate;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "date")
+    private XMLGregorianCalendar creationDate;
 
     public long getId() {
         return id;
@@ -33,11 +44,11 @@ public class Entry {
         this.content = content;
     }
 
-    public Date getCreationDate() {
+    public XMLGregorianCalendar getCreationDate() {
         return creationDate;
     }
 
-    public void setCreationDate(Date creationDate) {
+    public void setCreationDate(XMLGregorianCalendar creationDate) {
         this.creationDate = creationDate;
     }
 
